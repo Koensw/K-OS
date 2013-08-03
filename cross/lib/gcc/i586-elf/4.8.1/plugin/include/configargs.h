e1100c1210c9371f85549936b874fa160591099c
Koen Wolters <k.s.wolters@gmail.com>
2013-08-03 18:38:37 +0200

    add gitignore

--- cross/lib/gcc/i586-elf/4.8.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../src/gcc/gcc-4.8.1/configure --target=i586-elf --prefix=/home/koen/kernel/cross --disable-nls --enable-languages=c,c++ --without-headers";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
